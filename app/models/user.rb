fb6dbde30b040354eaa7b0024f088bad3bbb1a9e
Tetsuya Yokota <tetsuya.myy2@gmail.com>
2023-05-12 14:49:47 +0900

    apply auto collect
    Signed-off-by: Tetsuya Yokota <tetsuya.myy2@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,3 @@ class User < ApplicationRecord
   validates :uid, presence: true, uniqueness: true
   validates :password_digest, presence: true
 end
-  
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
-    factory :user do
-      sequence :uid do |n|
-        "uid-#{n}"
-      end
-      password { 'password' }
+  factory :user do
+    sequence :uid do |n|
+      "uid-#{n}"
     end
+    password { 'password' }
   end
-  
+end
