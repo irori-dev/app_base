52d44d8d7aa71c08ae376ecf4e11a13c554811e2
Kenji ADACHI <kenji.183927@gmail.com>
2023-05-12 16:58:10 +0900

    Update app/controllers/sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,11 @@
 class SessionsController < ApplicationController
-  def index; end
-
   def new
     @user = User.new
   end
 
-  def edit; end
-
   def create
-    @user = User.new(user_params)
-    user = User.find_by(uid: params[:user][:uid])
-    if user&.authenticate(params[:user][:password])
+    user = User.find_by(uid: permit_params[:uid])
+    if user&.authenticate(permit_params[:password])
       session[:uid] = user.uid
       redirect_to root_path
     else
@@ -19,15 +14,9 @@ class SessionsController < ApplicationController
     end
   end
 
-  def update; end
-
-  def destroy
-    flash.now.notice = 'ログアウトしました。'
-  end
-
   private
 
-  def user_params
+  def permit_params
     params.require(:user).permit(:uid, :password)
   end
 end
