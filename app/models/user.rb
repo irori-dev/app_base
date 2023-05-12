da6f6d7503d09bcbd4dda53ccd580065d3bbe12e
Tetsuya Yokota <tetsuya.myy2@gmail.com>
2023-05-12 14:45:00 +0900

    fix spell
    Signed-off-by: Tetsuya Yokota <tetsuya.myy2@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :uid, presence: true, uniqueness: true
-  validates :passowrd_digest, presence: true
+  validates :password_digest, presence: true
 end
   
