e1d56442a7a7a6e95a7f7d4f3111bf9d4f76559b
Tetsuya Yokota <tetsuya.myy2@gmail.com>
2023-05-12 13:41:29 +0900

    fix index
    Signed-off-by: Tetsuya Yokota <tetsuya.myy2@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password
-    validates :uid, presence: true, uniqueness: true
-    validates :passowrd_digest, presence: true
+  has_secure_password
+  validates :uid, presence: true, uniqueness: true
+  validates :passowrd_digest, presence: true
 end
   
