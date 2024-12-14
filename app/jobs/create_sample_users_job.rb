745ec04f1f91ef6fa66efa55487fac8a3dc2cc13
Kenji ADACHI <kenji.183927@gmail.com>
2024-12-14 20:54:48 +0900

    rubocop

--- app/jobs/create_sample_users_job.rb
+++ app/jobs/create_sample_users_job.rb
@@ -3,9 +3,9 @@ class CreateSampleUsersJob < ApplicationJob
 
   def perform(*args)
     count = args[0]
-    chars = ('a'..'z').to_a
+    chars = ("a".."z").to_a
     count.times do |i|
-      User::Core.create(email: "#{8.times.map{ chars.sample }.join}@example.com", password: "password")
+      User::Core.create(email: "#{8.times.map { chars.sample }.join}@example.com", password: "password")
       puts "Created #{i + 1} users"
       sleep 3
     end
