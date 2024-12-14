565a2c07e76331803256d82646915f1ede60a3c4
Kenji ADACHI <kenji.183927@gmail.com>
2024-12-14 23:34:45 +0900

    rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -31,5 +31,5 @@ Rails.application.routes.draw do
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   mount MissionControl::Jobs::Engine, at: "/admins/jobs"
-  mount ActionCable.server => '/cable'
+  mount ActionCable.server => "/cable"
 end
