1892467eeef3dc7621baa6f5a7df7f1558ff2f50
Kenji ADACHI <kenji.183927@gmail.com>
2024-12-15 00:39:56 +0900

    rubocop

--- spec/components/users/sidebar_spec.rb
+++ spec/components/users/sidebar_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Users::Sidebar::Component, type: :component do
   subject { render_inline(described_class.new) }
-  
+
   it 'renders change email link' do
     subject
     expect(page).to have_link 'メールアドレス変更', href: '/users/email_changes/new'
