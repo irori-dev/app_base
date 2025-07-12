e0be619514f404a87ccca8de95ca71797e85328f
Kenji ADACHI <kenji.183927@gmail.com>
2025-07-12 11:26:32 +0900

    rubocop

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -46,6 +46,6 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--disable-gpu')
   options.add_argument('--window-size=1680,1050')
-  
+
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
